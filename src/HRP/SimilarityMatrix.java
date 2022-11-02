c7407b1e9074730461b8aba70410868f56727224
One234Fi <54912519+One234Fi@users.noreply.github.com>
2022-11-02 12:23:32 -0500

    Implement Similarity Matrix

--- src/HRP/SimilarityMatrix.java
+++ src/HRP/SimilarityMatrix.java
@@ -3,7 +3,12 @@
  */
 package HRP;
 
+import java.util.TreeSet;
+import java.util.StringTokenizer;
+
 /**
+ * TODO: rank sentences, add TF-IDF?
+ * 
  * matrix of X sentences by Y sentences. any value of [n, n] will be 1.0
  * for values of [n, m], calculate with cosineSimilarity(sentenceN, sentenceM)
  * 
@@ -33,9 +38,74 @@ public class SimilarityMatrix {
         }
     }
     
+    //this probably could be broken up into a couple separate methods for readability
+    //ALSO wow this is some garbage time scaling
     private double cosineSimilarity(String a, String b) {
+        TreeSet<String> tempSet = new TreeSet<String>();
+        StringTokenizer st = new StringTokenizer(a + " " + b, " ");
         
-        return 0.0;
+        while (st.hasMoreTokens()) {
+            //String temp = st.nextToken();
+            //System.out.println(temp);
+            tempSet.add(st.nextToken());
+        }
+        
+        
+        String [] sentence_1 = a.split(" ");
+        String [] sentence_2 = b.split(" ");
+        int[] vector_A = new int[tempSet.size()];
+        int[] vector_B = new int[tempSet.size()];
+        
+        int i = 0;
+        String temp;
+        while (!tempSet.isEmpty()) {
+            temp = tempSet.pollFirst();
+            
+            int c1 = 0;
+            for (String s : sentence_1) {
+                if (temp.equalsIgnoreCase(s)) {
+                    c1++;
+                }
+            }
+            
+            int c2 = 0;
+            for (String s : sentence_2) {
+                if (temp.equalsIgnoreCase(s)) {
+                    c2++;
+                }
+            }
+            
+            vector_A[i] = c1;
+            vector_B[i] = c2;
+        }
+        
+        //Formula (A.B) / (||A||*||B||)
+        double cosineSim = dotProduct(vector_A, vector_B) / (magnitude(vector_A) * magnitude(vector_B));
+        
+        return cosineSim;
+    }
+    
+    //A and B must be the same length for this
+    private double dotProduct(int[] A, int[] B) {
+        double DP = 0.0;
+        
+        for (int i = 0; i < A.length; i++) {
+            DP += (A[i] * B[i]);
+        }
+        
+        return DP;
+    }
+    
+    private double magnitude (int[] vector) {
+        double result = 0.0;
+        
+        for (int i : vector) {
+            result += Math.pow(i, 2);
+        }
+        
+        result = Math.sqrt(result);
+        
+        return result;
     }
     
 }
